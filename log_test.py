2cae4178bf4400ec7d43129e22a2b5277cc7f331
Erfan <erfan@Erfankam-linux-0701.(none)>
2012-03-28 07:03:46 +0430

    crashed

--- log_test.py
+++ log_test.py
@@ -6,20 +6,20 @@ d = [6, 6, [{'Erfan':'Erfan', 'Qazal':'Qazal'}, {'Ali':'Mohammmad', 'Erfan':'Mah
 d1 = [6, 6, {'Erfan':'Erfan', 'Qazal':'Qazal'}, {'Ali':'Mohammmad', 'Erfan':'Mahdi'}, 3 , 5, 6]
 d={'Erfan':'Erfan', 'Qazal':'Qazal'} 
 d1={'Erfan':'Erfan', 'Qazal':'Qazal'}
-ekl = EKlogger("Qazal logger module test suite and its argument")
+ekl = Qlogger("Qazal logger module test suite and its argument")
 ekh = FileHandler('f.log', '', ' | date time: must affect>>> %dt\n | platform name: %pn \n | module name: %mn \n | class name: %cn\n \
 | log name: %ln \n | log object: %lo \n | function name: %fn\n | document string: %ds\n | platform name: %pn \n | log type: %lt\n \
 | standard input: %si\n | standard output: %so \n | standard error: %se\n | thread id: %ti\n | process id: %pi\n | user log: %ul\n \
 | os version: %ov\n | file address: %fa| os arch: %oa \n | os family: %of\n | os name: %on\n | host name: %hn\n >>> test: %dt%%%%dt  %ti%ti%ti')
-ekl.setLogLevel(3)
-ekl.addHandler(ekh)
+qkl.setLogLevel(3)
+qkl.addHandler(ekh)
 
 class a():
 	def a1(self):
 		print "this is a test"
-		ekl.Qlog('This what i mean', None, 4)
+		qkl.Qlog('This what i mean', None, 4)
 
 aobj = a()
 aobj.a1()
-ekl.Qlog("je suis pret  \n %q \n ################################# \nand its args is \n %q \n", (d1, d))
+qkl.Qlog("je suis pret  \n %q \n ################################# \nand its args is \n %q \n", (d1, d))
 exit()
